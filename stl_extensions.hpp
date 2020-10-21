bb0ccbfa620f08031ff030910800b1b5b4fb54ca
Anon Ymous <rbnelr@gmx.de>
2020-10-21 17:57:54 +0200

    improved stuff

--- stl_extensions.hpp
+++ stl_extensions.hpp
@@ -1,30 +1,37 @@
 #include <vector>
 
 namespace kiss {
-	template <typename T, typename EQUAL>
-	int indexof (std::vector<T>& vec, T& r, EQUAL are_equal) {
+	// returns lowest index where are_equal(vec[i], r) returns true or -1 if none are found
+	// bool are_equal(VT const& l, T const& r)
+	template <typename VT, typename T, typename EQUAL>
+	int indexof (std::vector<VT>& vec, T& r, EQUAL are_equal) {
 		for (int i=0; i<(int)vec.size(); ++i)
 			if (are_equal(vec[i], r))
 				return i;
 		return -1;
 	}
-	template <typename T>
-	int indexof (std::vector<T>& vec, T& r) {
+
+	// returns lowest index where (vec[i] == r) returns true or -1 if none are found
+	template <typename VT, typename T>
+	int indexof (std::vector<VT>& vec, T& r) {
 		for (int i=0; i<(int)vec.size(); ++i)
 			if (vec[i] == r)
 				return i;
 		return -1;
 	}
-	
-	template <typename T, typename EQUAL>
-	bool contains (std::vector<T>& vec, T& r, EQUAL are_equal) {
+
+	// returns true if any are_equal(vec[i], r) returns true
+	// bool are_equal(VT const& l, T const& r)
+	template <typename VT, typename T, typename EQUAL>
+	bool contains (std::vector<VT>& vec, T& r, EQUAL are_equal) {
 		for (auto& i : vec)
 			if (are_equal(i, r))
 				return true;
 		return false;
 	}
-	template <typename T, typename EQUAL>
-	bool contains (std::vector<T>& vec, T& r) {
+	// returns true if any (vec[i] == r) returns true
+	template <typename VT, typename T>
+	bool contains (std::vector<VT>& vec, T& r) {
 		for (auto& i : vec)
 			if (i == r)
 				return true;
